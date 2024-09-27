9d2e769435e2f12e65a0a701095b556597cb7f55
Cavalcante85 <scavalca85@gmai.com>
2024-09-27 19:03:46 -0300

    subindo projeto

---
+++ virtual_store1/src/main/java/cavalcante/deVirtual_store/virtual_store1/VirtualStore1Application.java
@@ -0,0 +1,13 @@
+package cavalcante.deVirtual_store.virtual_store1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualStore1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualStore1Application.class, args);
+	}
+
+}
---
+++ virtual_store1/src/test/java/cavalcante/deVirtual_store/virtual_store1/VirtualStore1ApplicationTests.java
@@ -0,0 +1,13 @@
+package cavalcante.deVirtual_store.virtual_store1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualStore1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
