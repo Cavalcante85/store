5ec114cb0ab7c41c40ff36a465013507ae6acb4b
Cavalcante85 <scavalca85@gmai.com>
2024-09-30 11:34:42 -0300

    Subir modelos de acesso

---
+++ virtual_store1/src/main/java/cavalcante/deVirtual_store/virtual_store1/models/PessoaJuridica.java
@@ -0,0 +1,69 @@
+package cavalcante.deVirtual_store.virtual_store1.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.PrimaryKeyJoinColumn;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_pessoa_juridica")
+//@PrimaryKeyJoinColumn(name = "id")
+public class PessoaJuridica extends Pessoa {
+    private static final long serialVersionUID = 1L;
+
+
+    private String cnpj;
+    private String razaoSocial;
+    private String categoria;
+    private String inscricaoEstadual;
+    private String inscricaoMunicipal;
+    private String nomeFantasia;
+
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getRazaoSocial() {
+        return razaoSocial;
+    }
+
+    public void setRazaoSocial(String razaoSocial) {
+        this.razaoSocial = razaoSocial;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getInscricaoEstadual() {
+        return inscricaoEstadual;
+    }
+
+    public void setInscricaoEstadual(String inscricaoEstadual) {
+        this.inscricaoEstadual = inscricaoEstadual;
+    }
+
+    public String getInscricaoMunicipal() {
+        return inscricaoMunicipal;
+    }
+
+    public void setInscricaoMunicipal(String inscricaoMunicipal) {
+        this.inscricaoMunicipal = inscricaoMunicipal;
+    }
+
+    public String getNomeFantasia() {
+        return nomeFantasia;
+    }
+
+    public void setNomeFantasia(String nomeFantasia) {
+        this.nomeFantasia = nomeFantasia;
+    }
+}
